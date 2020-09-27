8390dcdcf401af05b46857ff1385a2c5e666f802
Wesley Goncalves <wesleynarutops2@hotmail.com>
2020-09-27 00:29:01 -0300

    Exception handing for delete(referential integrity)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -36,9 +36,14 @@ namespace SalesWebMvc.Services
 
         public async Task RemoveAsync(int id)
         {
+            try { 
             var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
             await _context.SaveChangesAsync();
+            }catch(DbUpdateException e)
+            {
+                throw new IntegrityException(e.Message);
+            }
         }
 
         public async Task UpdadteAsync(Seller obj)
